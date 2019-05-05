f472955d3d6ba0cb70d98db9fff65a22641add16
Gereon Such <gereonsuch@gmail.com>
2019-05-05 20:41:16 +0200

    Added MessageHandler class, which inherits gnuradio sync_block.
    It receives messages as polymorphic types, distributes them among processing units and emits the output as messages. See docstrings for more information.
    
    Due to Gnuradio licensing, unfortunately, it has to be licensed as GPL v3.
    
    TODO:
     - More doc
     - Unit Test

--- messagehandler.h
+++ messagehandler.h
@@ -20,9 +20,109 @@
 #ifndef MESSAGEHANDLER_H
 #define MESSAGEHANDLER_H
 
+#include <gnuradio/sync_block.h>
+#include <pmt/pmt.h>
+#include <map>
+#include <string>
+#include <mutex>
+
+#include "processingunit.h"
+#include "pmttools.h"
+
 namespace gs {
 
+template<typename inT>
+class MessageHandler : virtual public gr::sync_block{
+    std::map<std::string, ProcessingUnit<pmt::pmt_t>* > d_processing_units;
 
+    pmt::pmt_t d_msgin;
+    pmt::pmt_t d_msgout;
+
+    std::mutex d_output_mutex;
+
+public:
+    MessageHandler() :
+        gr::sync_block("message_handler",
+                       gr::io_signature::make(0, 0, 0),
+                       gr::io_signature::make(0, 0, 0)),
+        d_msgin(pmt::intern("msgin")),
+        d_msgout(pmt::intern("msgout"))
+    {
+        //register in and out ports and bind the handle_input method to all incoming inputs.
+        message_port_register_in(d_msgin, std::bind(&MessageHandler<inT>::handle_input, this, _1));
+        message_port_register_out(d_msgout);
+    }
+
+    ~MessageHandler() {
+        //make all processing unit exit, since the process method is executed in a seperate thread.
+        std::map<std::string, ProcessingUnit<pmt::pmt_t>* >::iterator i;
+
+        for(i=d_processing_units.begin();i!=d_processing_units.end();i++){
+            try {
+                i->second->set_suicidal(true); //makes the object delete themselves after finishing
+                i->second->set_finished(); //makes the process routine run by the Processing unit exit after completion
+            } catch (std::exception &e) {}
+        }
+    }
+
+    void emit_msg(pmt::pmt_t msg){
+        /** \brief Emits the message on the output port.
+         *
+         * This method is bound to all ProcessingUnits. Internal mutex and guard locks are implemented to make it thread safe.
+         */
+
+        std::lock_guard<std::mutex> lock(d_output_mutex);
+        message_port_pub(d_msgout, msg);
+    }
+
+protected:
+    virtual ProcessingUnit<pmt::pmt_t>* new_processingunit(){
+        /** \brief Return a new ProcessingUnit instance.
+         *
+         * Overload this to create your Custom Processingunit and pass default parameters to it.
+         *
+         * DO NOT FORGET TO PASS EMIT FUNCTION!
+         *
+         * Defaults to: return new ProcessingUnit<pmt::pmt_t>(std::bind(&MessageHandler<inT>::emit_msg, this, _1));
+         */
+
+        return new ProcessingUnit<pmt::pmt_t>(std::bind(&MessageHandler<inT>::emit_msg, this, _1));
+    }
+
+    void handle_input(pmt::pmt_t msg){
+        /** \brief Input method for all messages.
+         *
+         * Distributes all messages on seperate Processing units based on the messages ID. .
+         * If no ProcessingUnit is found, a new one is created.
+         */
+
+        //get ID
+        std::string ID=pmttools::get_param(msg, "ID", std::string("None"));
+        bool finished=pmttools::get_param(msg, "finished", true); /* if finished flag does not exist, force it true
+                                                                   * by default value to make the thread finish. */
+
+        //check if ID is in map or add new if it doesnt exist.
+        std::map<std::string, ProcessingUnit<pmt::pmt_t>* >::iterator i=d_processing_units.find(ID);
+
+        //if false, add new processing unit.
+        if(i==d_processing_units.end()){
+            ProcessingUnit<pmt::pmt_t> *unit=new_processingunit(); //create new instance
+
+            //add new instance to the map
+            std::pair<std::map<std::string, ProcessingUnit<pmt::pmt_t>* >::iterator,bool> p =
+                    d_processing_units.insert(std::pair<std::string, ProcessingUnit<pmt::pmt_t>* >(ID, unit ) );
+
+            if(p.second==false)
+                return; //abort if Processing unit can not be inserted. This should not happen...
+
+            i=p.first; //set iterator to the newly inserted processing unit
+        }
+
+        //add data to the processing unit.
+        i->second->push(msg, finished);
+    }
+
+};
 
 } //namespace gs
 
